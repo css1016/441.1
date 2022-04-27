8a086a7ec2de292628a4be835f0e4932040c76c8
css1016 <103395928+css1016@users.noreply.github.com>
2022-04-27 16:48:02 +0800

    Update a.js

--- a.js
+++ a.js
@@ -15,3 +15,29 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";  
 }
+
+
+function validateform(){  
+var name=document.myform.name.value;  
+var gender=document.myform.gende.value;  
+var problem=document.myform.problem.value;  
+var telephonenumber=document.myform.telephonenumber.value;  
+if (name==null || name=="")
+{  
+  alert("Name can't be blank");  
+  return false;  
+ }
+else if(gender==null || gender==""){  
+  alert("gender must be at least 6 characters long.");  
+  return false;  
+  }  
+else if(problem==null || problem==""){  
+    alert("problem must be at least 6 characters long.");  
+    return false; 
+  }
+else if(telephonenumber.length<6){  
+    alert("telephonenumber must be at least 6 characters long.");  
+    return false; 
+  } 
+
+}  
